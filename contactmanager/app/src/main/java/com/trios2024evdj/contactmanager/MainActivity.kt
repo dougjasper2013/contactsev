0313b4ff3ac34f9132a2eb88cc84ebfaa32d91ef
Douglas Jasper <doug.jasper@trios.com>
2024-08-19 20:29:03 -0400

    Added Dialog

--- contactmanager/app/src/main/java/com/trios2024evdj/contactmanager/MainActivity.kt
+++ contactmanager/app/src/main/java/com/trios2024evdj/contactmanager/MainActivity.kt
@@ -2,17 +2,51 @@ package com.trios2024evdj.contactmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.trios2024evdj.contactmanager.databinding.ActivityMainBinding
 import com.trios2024evdj.contactmanager.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.fabButton.setOnClickListener {
+            showCreateListDialog()
+        }
+
     }
+
+    private fun showCreateListDialog() {
+        val dialogTitle = getString(R.string.contact_name)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        val builder = AlertDialog.Builder(this)
+        val listContactEditText = EditText(this)
+        listContactEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listContactEditText)
+
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
+    }
+
 }
